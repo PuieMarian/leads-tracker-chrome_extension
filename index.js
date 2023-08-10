496482480d174f2b7ec2575240da572761dd7bc3
Marian Puie <60893978+PuieMarian@users.noreply.github.com>
2023-08-10 12:04:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+let myLeads = [];
+let oldLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+// 1. Store the delete button in a deleteBtn variable
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+};
+
+tabBtn.addEventListener("click", function(){
+     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+     });
+    
+})
+
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `;
+    };
+    ulEl.innerHTML = listItems;
+};
+
+// 2. Listen for double clicks on the delete button (google it!)
+deleteBtn.addEventListener("dblclick", function(){
+    // 3. When clicked, clear localStorage, myLeads, and the DOM
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) );
+    render(myLeads);
+})
